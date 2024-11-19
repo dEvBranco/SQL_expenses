c2c9c630f4026865ec3e2d2cf86ccef489252230
dEvBranco <emfbranco@gmail.com>
2024-11-19 21:45:17 +0000

    Update_1

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
 # App design
+from PyQt6.QtWidgets import (
+    QWidget,
+    QLabel,
+    QPushButton,
+    QLineEdit,
+    QComboBox,
+    QDateEdit,
+    QTableWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QTableWidgetItem,
+    QHeaderView,
+)
+
+from PyQt6.QtCore import QDate, Qt
+
+
+class ExpenseApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.settings()
+
+    def settings(self):
+        self.setGeometry(300, 300, 550, 500)
+        self.setWindowTitle("Expenses Tracker")
+
+    # Design
+    def initUI(self):
+        # Create all objects
+        self.date_box = QDateEdit()
+        self.date_box.setDate(QDate.currentDate())
+        self.dropdown = QComboBox()
+        self.amount = QLineEdit()
+        self.description = QLineEdit()
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # Running the app
-from PyQt6.QtWidgets import (
-    QWidget,
-    QLabel,
-    QPushButton,
-    QLineEdit,
-    QComboBox,
-    QDateEdit,
-    QTableWidget,
-    QVBoxLayout,
-    QHBoxLayout,
-    QMessageBox,
-    QTableWidgetItem,
-    QHeaderView,
-)
+import sys
+from PyQt6.QtWidgets import QApplication, QMessageBox
+from app import ExpenseApp
+
+
+def main():
+    app = QApplication(sys.argv)  # Also could be QApplication([])
+
+    window = ExpenseApp()
+    window.show()
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
