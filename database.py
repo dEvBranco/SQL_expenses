e045f7d49783993418de27724e71c13eb70b06d0
dEvBranco <emfbranco@gmail.com>
2024-11-19 21:19:23 +0000

    FirstCommit

---
+++ database.py
@@ -0,0 +1,1 @@
+# All sql stuff here
---
+++ main.py
@@ -0,0 +1,15 @@
+# Running the app
+from PyQt6.QtWidgets import (
+    QWidget,
+    QLabel,
+    QPushButton,
+    QLineEdit,
+    QComboBox,
+    QDateEdit,
+    QTableWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QMessageBox,
+    QTableWidgetItem,
+    QHeaderView,
+)
