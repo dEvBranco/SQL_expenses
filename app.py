22148a17cb64bc04a39b0485510953d2418a5756
dEvBranco <emfbranco@gmail.com>
2024-11-20 16:12:14 +0000

    save37:49

--- app.py
+++ app.py
@@ -21,6 +21,7 @@ class ExpenseApp(QWidget):
     def __init__(self):
         super().__init__()
         self.settings()
+        self.initUI()
 
     def settings(self):
         self.setGeometry(300, 300, 550, 500)
@@ -34,3 +35,51 @@ class ExpenseApp(QWidget):
         self.dropdown = QComboBox()
         self.amount = QLineEdit()
         self.description = QLineEdit()
+
+        self.btn_add = QPushButton("Add Expense")
+        self.btn_delete = QPushButton("Delete")
+
+        self.table = QTableWidget(0, 5)
+        self.table.setHorizontalHeaderLabels(
+            ["ID", "Date", "Category", "Amount", "Description"]
+        )
+        self.table.horizontalHeader().setSectionResizeMode(
+            QHeaderView.ResizeMode.Stretch
+        )
+
+        self.populate_dropdown()
+
+        # Add widgets to a layout (row/column)
+        self.setup_layout()
+
+    def setup_layout(self):
+        master = QVBoxLayout()
+        row1 = QHBoxLayout()
+        row2 = QHBoxLayout()
+        row3 = QHBoxLayout()
+
+        # Row 1
+        row1.addWidget(QLabel("Date"))
+        row1.addWidget(self.date_box)
+        row1.addWidget(QLabel("Category"))
+        row1.addWidget(self.dropdown)
+
+        # Row 2
+        row2.addWidget(QLabel("Amount"))
+        row2.addWidget(self.amount)
+        row2.addWidget(QLabel("Description"))
+        row2.addWidget(self.description)
+
+        row3.addWidget(self.btn_add)
+        row3.addWidget(self.btn_delete)
+
+        master.addLayout(row1)
+        master.addLayout(row2)
+        master.addLayout(row3)
+        master.addWidget(self.table)
+
+        self.setLayout(master)
+
+    def populate_dropdown(self):
+        categories = ["Food", "Rent", "Bills", "Entertainment", "Shopping", "Other"]
+        self.dropdown.addItems(categories)
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 # Running the app
 import sys
 from PyQt6.QtWidgets import QApplication, QMessageBox
+from database import init_db
 from app import ExpenseApp
 
 
 def main():
     app = QApplication(sys.argv)  # Also could be QApplication([])
+
+    if not init_db("expense.db"):
+        QMessageBox.critical(None, "Error", "Could not load your database...")
+        sys.exit(1)
 
     window = ExpenseApp()
     window.show()
